d9168ade80a868b8b1aaff6bfa014955df6c3265
VieyKer <55123502+VieyKer@users.noreply.github.com>
2020-05-22 22:17:16 +0300

    Lab+MedicalIndices

---
+++ src/Model/MedicalIndices.java
@@ -0,0 +1,59 @@
+package Model;
+
+public class medicalIndices {
+	private int weight, height, heartRate, temperature, systolicBP,diastolicBP;
+	
+	
+	public void setWeight(int wParmeter) {
+		this.weight = wParmeter;
+			
+	}
+		
+	public void setHeight(int hrParmeter) {
+		this.height = hrParmeter;
+	}
+	
+	public void setHeartRate(int hParmeter) {
+		this.heartRate = hParmeter;
+	}
+		
+	public void setTemperature (int tParmeter) {
+		this.temperature = tParmeter;
+	}
+	
+	public void setSystolicBP (int sbpParmeter) {
+		this.systolicBP = sbpParmeter;
+	}
+	
+	public void setDiastolicBP (int dbpParmeter) {
+		this.diastolicBP = dbpParmeter;
+	}
+
+	
+	public int getWeight() {
+		return this.weight;
+			
+	}
+		
+	public int getHeight(){
+		return this.height;
+	}
+	
+	public int getHeartRate() {
+		return this.heartRate;
+	}
+		
+	public int getTemperature () {
+		return this.temperature;
+	}
+	
+	public int getSystolicBP() {
+		return this.systolicBP;
+	}
+	
+	public int getDiastolicBP () {
+		return this.diastolicBP;
+	}
+	
+
+}
