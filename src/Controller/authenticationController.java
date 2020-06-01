9b8b04224445ab6c5be3162a8ad6f6912c25793f
barlanyado <65768646+barlanyado@users.noreply.github.com>
2020-06-01 20:28:15 +0300

    Update authenticationController.java

--- src/Controller/authenticationController.java
+++ src/Controller/authenticationController.java
@@ -19,9 +19,27 @@ public class AuthenticationController{
 
     public String login(String id, String password)
     {
-        if (isNumeric(id))
+        if (isNumeric(id) && id.length() == 9)
             return Authentication.signIn(id,password);
+        else
+            return "ID can contain 9 digits only";
     }
 
+    /*public String isAuthorized(String token, String typeNeed) // we need to figure where the typeNeed comes from / maybe this function can be deleted.
+    {
+        if(Authentication.validateUser(token,typeNeed))
+            return "true";
+        
+        return false;
+    } */
+
+    public String changePassword(String token,String cPassword,String nPassword )
+    {
+      if(Authentication.resetPassword(token,cPassword,nPassword))
+        return "Password changed successfuly";
+      
+      else
+        return "Wrong current password, password did not changed";
+    }
 
 }
