62bfb3c787144601166d2af9e0888b3cca192665
Lior Ben Dayan <liorbendayan@gmail.com>
2020-06-05 22:09:28 +0300

    Deleting files

--- src/Hello.java
+++ src/Hello.java
@@ -1,13 +1,7 @@
-import View.*;
+//import View.*;
 
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        DoctorDetailsView dv = new DoctorDetailsView();
-        LoginView lv = new LoginView();
-        AddDoctorView ad = new AddDoctorView();
-        StatisticalReportView asd = new StatisticalReportView();
-        DoctorPanelView sss = new DoctorPanelView();
-        ManagerPanelView aasdasd = new ManagerPanelView();
 	}
 }
