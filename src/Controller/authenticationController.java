8489e1bd079d8b96ce5dff25f1af0af52c9aa3a3
Lior Ben Dayan <liorbendayan@gmail.com>
2020-06-06 17:19:24 +0300

    Merge branch 'bar_branch' into lior_branch

---
+++ src/Controller/authenticationController.java
@@ -0,0 +1,53 @@
+package Controller;
+
+import Model.Authentication
+import java.util.*
+public class AuthenticationController{
+
+
+    private static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public String login(String id, String password)
+    {
+        if (isNumeric(id) && id.length() == 9)
+            return Authentication.signIn(id,password);
+        else
+            return "ID can contain 9 digits only";
+    }
+
+    /*public String isAuthorized(String token, String typeNeed) // we need to figure where the typeNeed comes from / maybe this function can be deleted.
+    {
+        if(Authentication.validateUser(token,typeNeed))
+            return "true";
+        
+        return false;
+    } */
+
+    public String changePassword(String token,String cPassword,String nPassword )
+    {
+      if(Authentication.resetPassword(token,cPassword,nPassword))
+        return "Password changed successfuly";
+      
+      else
+        return "Wrong current password, password did not changed";
+    }
+
+    public String logOut(String token)
+    {
+        if (Authentication.signOut(token))
+            return "Singed out";
+        else
+            return "user not found";
+    }
+
+}
