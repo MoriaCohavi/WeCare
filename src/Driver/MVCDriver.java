b35481cd43fb66cc85e14211e3a5a3335650716e
Lior Ben Dayan <liorbendayan@gmail.com>
2020-06-13 13:11:43 +0300

    commit 2

--- src/Driver/MVCDriver.java
+++ src/Driver/MVCDriver.java
@@ -5,15 +5,19 @@ import Controller.*;
 import Model.*;
 
 public class MVCDriver {
+	
 	public static void main(String[] args) {
 		serHandlerController handler = new serHandlerController();
 		//authenticationController auth_controller = new authenticationController();
 		clinicController clinic_controller = new clinicController("", 0);
 		clinic_controller.deserialize(handler);
-		managerController manager_controller = new managerController();
-		manager_controller.deserialize(handler); // Id = 111111111, password = admin
-		manager_controller.registerManager();
 		
+		/* Before finish, do serialize to manager */
+//		managerController manager_controller = new managerController();
+//		manager_controller.deserialize(handler); // Id = 111111111, password = admin
+		
+		managerController manager_controller = new managerController("111111111", 548798888, "Moshe Bar Siman Tov", "moshe@maccabi.org", "admin", "Manager");
+		manager_controller.registerManager();
 		authenticationController auth_controller = new authenticationController();
 		
 	}
